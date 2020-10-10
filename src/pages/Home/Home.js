a4fe0597817efa83ab10d65068fa9f8ee4cad9fb
windcloak <xfirecloak@gmail.com>
2020-10-10 16:31:56 -0500

    Added api calls to get series and amiibo

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,21 +1,50 @@
-import React from 'react';
-// import React, { useEffect, useState } from 'react';
-
+import React, { useEffect, useState } from 'react';
 // import './Home.scss';
-import { Button } from 'react-bootstrap';
+import axios from 'axios';
 
 function Home() {
+  const [seriesLoading, setSeriesLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [series, setSeries] = useState([]);
+  const [amiibos, setAmiibos] = useState([]); // amiibos by series
+  const [seriesTerm, setSeriesTerm] = useState('Super Smash Bros.');  // series to show
+
+  const API_SERIES_URL = 'https://www.amiiboapi.com/api/amiiboseries';
+  const API_AMIIBOS_URL = 'https://www.amiiboapi.com/api/amiibo/';
+
+  // get all series
+  useEffect(() => {
+    axios
+    .get(API_SERIES_URL)
+    .then(res => {
+      console.log(res.data);
+      setSeries(res.data);
+      setSeriesLoading(false);
+    })
+    .catch(err => {
+      console.log(err);
+      setError(true);
+    })
+  }, []);
+
+  // get all amiibos in the series
+  useEffect(() => {
+    axios
+    .get(`${API_AMIIBOS_URL}?amiiboSeries=${seriesTerm}`)
+    .then(res => {
+      console.log(res.data);
+      setAmiibos(res.data);
+      // setSeriesLoading(false);
+    })
+    .catch(err => {
+      console.log(err);
+      setError(true);
+    })
+  }, [seriesTerm]); // only rerun if series term changes
+
     return (
         <div>This is the Home<br/>
-<>
-  <Button variant="primary">Primary</Button>{' '}
-  <Button variant="secondary">Secondary</Button>{' '}
-  <Button variant="success">Success</Button>{' '}
-  <Button variant="warning">Warning</Button>{' '}
-  <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
-  <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
-  <Button variant="link">Link</Button>
-</>
+
 </div>
     );
   }
