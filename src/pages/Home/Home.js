f7cc19795b443597f4f9e920b322080d2e810ef7
windcloak <xfirecloak@gmail.com>
2020-10-10 21:26:25 -0500

    User can change series

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,51 +1,98 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 // import './Home.scss';
-import axios from 'axios';
+import axios from "axios";
+import { Row, Col, Container, Card, CardColumns, Dropdown } from "react-bootstrap";
+import DropdownButton from "react-bootstrap/DropdownButton";
 
 function Home() {
   const [seriesLoading, setSeriesLoading] = useState(true);
   const [error, setError] = useState(false);
-  const [series, setSeries] = useState([]);
-  const [amiibos, setAmiibos] = useState([]); // amiibos by series
-  const [seriesTerm, setSeriesTerm] = useState('Super Smash Bros.');  // series to show
+  const [series, setSeries] = useState({ amiibo: [] });
+  const [amiibos, setAmiibos] = useState({ amiibo: [] }); // amiibos by series
+  const [seriesTerm, setSeriesTerm] = useState("Super Smash Bros."); // series to show
 
-  const API_SERIES_URL = 'https://www.amiiboapi.com/api/amiiboseries';
-  const API_AMIIBOS_URL = 'https://www.amiiboapi.com/api/amiibo/';
+  const API_SERIES_URL = "https://www.amiiboapi.com/api/amiiboseries";
+  const API_AMIIBOS_URL = "https://www.amiiboapi.com/api/amiibo/";
 
   // get all series
   useEffect(() => {
     axios
-    .get(API_SERIES_URL)
-    .then(res => {
-      console.log(res.data);
-      setSeries(res.data);
-      setSeriesLoading(false);
-    })
-    .catch(err => {
-      console.log(err);
-      setError(true);
-    })
+      .get(API_SERIES_URL)
+      .then((res) => {
+        // console.log(res.data);
+        setSeries(res.data);
+        setSeriesLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setError(true);
+      });
   }, []);
 
   // get all amiibos in the series
   useEffect(() => {
     axios
-    .get(`${API_AMIIBOS_URL}?amiiboSeries=${seriesTerm}`)
-    .then(res => {
-      console.log(res.data);
-      setAmiibos(res.data);
-      // setSeriesLoading(false);
-    })
-    .catch(err => {
-      console.log(err);
-      setError(true);
-    })
+      .get(`${API_AMIIBOS_URL}?amiiboSeries=${seriesTerm}`)
+      .then((res) => {
+        console.log(res.data);
+        setAmiibos(res.data);
+        // setSeriesLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setError(true);
+      });
   }, [seriesTerm]); // only rerun if series term changes
 
+  const seriesItem = series.amiibo.map((data, i) => {
     return (
-        <div>This is the Home<br/>
-
-</div>
+      <Dropdown.Item
+        as="button"
+        key={i}
+        eventKey={data.name}
+        onChange={(e) => setSeriesTerm(data.name)}
+      >
+        {data.name}
+      </Dropdown.Item>
     );
-  }
+  });
+
+const amiiboCard = amiibos.amiibo.map((amiibo,i) => {
+  return (
+    <Card key={i} className="text-center m-5">
+    <Card.Img variant="top" src={amiibo.image} />
+    <Card.Body>
+      <Card.Title>{amiibo.character}</Card.Title>
+      {/* <Card.Text>Cases {data.cases}</Card.Text> */}
+    </Card.Body>
+  </Card>
+  );
+});
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Amiibo</h1>
+
+          <DropdownButton id="dropdown-basic-button" title="Choose Series">
+            {series && seriesItem}
+          </DropdownButton>
+
+          <button onClick={(e) => setSeriesTerm("Super Mario Bros.")}>
+            click me to change
+          </button>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col>
+          <CardColumns>
+            { amiiboCard }
+          </CardColumns>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
 export default Home;
