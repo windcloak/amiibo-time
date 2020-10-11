98d67bc143045e7bba6739f59ba9304d15eefce0
windcloak <xfirecloak@gmail.com>
2020-10-11 01:20:25 -0500

    Added search for character feature

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,23 @@
 import React, { useEffect, useState } from "react";
-import './Home.scss';
+import "./Home.scss";
 import axios from "axios";
-import { Row, Col, Container, Card, CardColumns, Dropdown, CardDeck } from "react-bootstrap";
+import {
+  Row,
+  Col,
+  Container,
+  Card,
+  CardColumns,
+  Dropdown,
+  Form,
+  Button,
+} from "react-bootstrap";
 import DropdownButton from "react-bootstrap/DropdownButton";
+import PacmanLoader from "react-spinners/PacmanLoader";
 
 function Home() {
   const [seriesLoading, setSeriesLoading] = useState(true);
+  const [loadingAmiibo, setLoadingAmiibo] = useState(true);
+  const [searchCharacter, setSearchCharacter] = useState("");
   const [error, setError] = useState(false);
   const [series, setSeries] = useState({ amiibo: [] });
   const [amiibos, setAmiibos] = useState({ amiibo: [] }); // amiibos by series
@@ -31,12 +43,13 @@ function Home() {
 
   // get all amiibos in the series
   useEffect(() => {
+    setLoadingAmiibo(true);
     axios
       .get(`${API_AMIIBOS_URL}?amiiboSeries=${seriesTerm}`)
       .then((res) => {
         console.log(res.data);
         setAmiibos(res.data);
-        // setSeriesLoading(false);
+        setLoadingAmiibo(false);
       })
       .catch((err) => {
         console.log(err);
@@ -44,49 +57,113 @@ function Home() {
       });
   }, [seriesTerm]); // only rerun if series term changes
 
+
+  // get character
+  useEffect(() => {
+    
+    if (searchCharacter==="")
+      return;
+
+    setLoadingAmiibo(true);
+    axios
+      .get(`${API_AMIIBOS_URL}?name=${searchCharacter}`)
+      .then((res) => {
+        console.log(res.data);
+        setAmiibos(res.data);
+        setLoadingAmiibo(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setError(true);
+        setLoadingAmiibo(false);
+      });
+  }, [searchCharacter]); // only rerun if character term changes
+
   const seriesItem = series.amiibo.map((data, i) => {
     return (
-      <Dropdown.Item
+      <option
+        value={data.name}
         key={i}
-        eventKey={data.name}
-        onSelect={(e) => setSeriesTerm(data.name)}
+        onChange={(e) => setSeriesTerm(data.name)}
       >
         {data.name}
-      </Dropdown.Item>
+      </option>
     );
   });
 
-const amiiboCard = amiibos.amiibo.map((amiibo,i) => {
-  return (
-    <Card key={i} className="text-center border-0 bg-transparent p-5">
-    <Card.Img variant="top" src={amiibo.image}/>
-    <Card.Body>
-      <Card.Title>{amiibo.character}</Card.Title>
-      {/* <Card.Text>Cases {data.cases}</Card.Text> */}
-    </Card.Body>
-  </Card>
-  );
-});
+  const amiiboCard = amiibos.amiibo.map((amiibo, i) => {
+    return (
+      <Card key={i} className="text-center border-0 bg-transparent p-5">
+        <Card.Img variant="top" src={amiibo.image} />
+        <Card.Body>
+          <Card.Title>{amiibo.character}</Card.Title>
+          {/* <Card.Text>Cases {data.cases}</Card.Text> */}
+        </Card.Body>
+      </Card>
+    );
+  });
+
+  const override = `
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
+
 
   return (
     <Container>
       <Row className="pb-4">
         <Col>
           <h1>Amiibo Time</h1>
+        </Col>
+      </Row>
 
-          <DropdownButton id="dropdown-basic-button" title="Choose Series" boundary="scroll-parent">
-            {series && seriesItem}
-          </DropdownButton>
+      <Row>
+        <Col className="d-flex justify-content-center">
+          <Form inline>
+            <Form.Label onSubmit={ (e) => setSearchCharacter(e.target.items.character.value)}
+              className="my-1 mr-2"
+              htmlFor="inlineFormCustomSelectPref"
+            >
+              Series
+            </Form.Label>
+            <Form.Control
+              as="select"
+              className="my-1 mr-sm-2"
+              id="inlineFormCustomSelectPref"
+              onChange={(e) => setSeriesTerm(e.target.value)}
+              custom
+            >
+              {seriesItem}
+            </Form.Control>
 
+            <Form.Label htmlFor="character" srOnly>
+              character
+            </Form.Label>
+            <Form.Control
+              placeholder="Search for Character"
+              name="character"
+              onChange={(e) => setSearchCharacter(e.target.value)}
+              required
+            />
+
+           <Button type="submit" className="my-1">
+              Submit
+            </Button> 
+          </Form>
         </Col>
       </Row>
 
       <Row>
         <Col className="d-grid justify-content-center">
-          <CardColumns>
-            { amiiboCard }
-          </CardColumns>
+          <PacmanLoader
+            css={override}
+            size={25}
+            color={"gold"}
+            loading={loadingAmiibo}
+          />
 
+          <CardColumns>{amiiboCard}</CardColumns>
         </Col>
       </Row>
     </Container>
