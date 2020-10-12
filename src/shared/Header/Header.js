ad70c19ced377e7ba58f2b00c267c3d9ffbe96f4
windcloak <xfirecloak@gmail.com>
2020-10-12 10:59:14 -0500

    Changed form control focus color

--- src/shared/Header/Header.js
+++ src/shared/Header/Header.js
@@ -18,14 +18,14 @@ function Header() {
         src={Logo}
         width="50"
         height="30"
-        className="d-inline-block align-top"
+        className="myimag d-inline-block align-top"
       />{' '}
     Amiibo Time</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="/about" title="About Website">About</Nav.Link>
-      <Nav.Link href="/amiibo" title="What are amiibo?">Amiibo</Nav.Link>
+      <Nav.Link href="/about" title="About Website" className="font-weight-bold">About</Nav.Link>
+      <Nav.Link href="/amiibo" title="What are amiibo?" className="font-weight-bold">Amiibo</Nav.Link>
     </Nav>
     {/* <Nav>
       <Nav.Link href="#deets">More deets</Nav.Link>
